0deb7df5f3d6d9732eae4f574d69e0bff2106439
KarnaukhovKirill <kirya2361@gmail.com>
2022-02-10 15:43:33 +0300

    add place validate in payment page

--- src/main/java/ru/job4j/cinema/controller/PaymentServlet.java
+++ src/main/java/ru/job4j/cinema/controller/PaymentServlet.java
@@ -17,31 +17,35 @@ import java.nio.charset.StandardCharsets;
 public class PaymentServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        var gson = (Gson) getServletContext().getAttribute("GSON");
         var place = req.getParameter("place");
         var id = req.getParameter("id");
         var session = req.getParameter("session");
         var username = req.getParameter("username");
         var email = req.getParameter("email");
         var phone = req.getParameter("phone");
-        resp.setContentType("application/json; charset=utf-8");
+        resp.setContentType("text/html; charset=utf-8");
         OutputStream outputStream = resp.getOutputStream();
+        String response;
         var store = DbStore.instOf();
-        var account = store.findAccountByPhone(phone);
-        if (account == null) {
-            account = new Account(0, username, email, phone);
-        } else {
-            account = new Account(account.getId(), username, email, phone);
-        }
-        store.saveAccount(account);
         var ticket = store.findTicketById(Integer.parseInt(id));
-        ticket.setAvailable(false);
-        ticket.setAccountId(account.getId());
-        store.saveTicket(ticket);
-        String response = "Кресло " + place.charAt(1) + " в " + place.charAt(0) + "-ом ряду зарезервировано для "
-                + account.getUsername() + ". Ждём Вас в " + session + " в нашем кинотеатре!";
-        String json = gson.toJson(response);
-        outputStream.write(json.getBytes(StandardCharsets.UTF_8));
+        if (ticket.isAvailable()) {
+            var account = store.findAccountByPhone(phone);
+            if (account == null) {
+                account = new Account(0, username, email, phone);
+            } else {
+                account = new Account(account.getId(), username, email, phone);
+            }
+            store.saveAccount(account);
+            ticket.setAvailable(false);
+            ticket.setAccountId(account.getId());
+            store.saveTicket(ticket);
+            response = "<p>Кресло " + place.charAt(1) + " в " + place.charAt(0) + "-ом ряду зарезервировано для "
+                    + account.getUsername() + ". Ждём Вас в " + session + " в нашем кинотеатре!</p>";
+        } else {
+            response = "<p>К сожалению, кресло " + place.charAt(1) + " в " + place.charAt(0) + "-ом ряду уже занято.</p>";
+        }
+        response += "<p><a href=\"/index.html\">На главную</a></p>";
+        outputStream.write(response.getBytes(StandardCharsets.UTF_8));
         outputStream.flush();
     }
 }
